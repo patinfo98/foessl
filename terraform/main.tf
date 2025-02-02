527445095c86a9c8fdb229d9d3981ecc67e70e2b
Patrick Foessl <patrick.foessl@edu.fh-joanneum.at>
2025-02-03 00:53:34 +0100

    add terraform script

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = ">= 1.35.0"
+    }
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_ssh_key" "contdel-key" {
+  name       = "contdel-key"
+  public_key = var.ssh_public_key
+}
+
+resource "hcloud_server" "contdel-server" {
+  name        = "contdel-server"
+  image       = "ubuntu-22.04"
+  server_type = "cx11"
+  location    = "nbg1"
+  ssh_keys    = [hcloud_ssh_key.contdel-key.id]
+}
