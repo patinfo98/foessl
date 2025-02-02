b671c47c71ca0d598f19b144df41de35baabca3e
Patrick Foessl <patrick.foessl@edu.fh-joanneum.at>
2025-02-03 00:53:34 +0100

    add terraform script

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "hcloud_token" {
+  description = "Hetzner Cloud API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH Public Key"
+  type        = string
+}
